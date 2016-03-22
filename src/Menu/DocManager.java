8841aa79f422d0c7a12caddd9acb0692542d9bac
Joao Barreiros <jgbbarreiros@gmail.com>
2016-03-22 20:22:54 +0000

    Document Manager
    Create, load and save documents with this new Document Manager

---
+++ src/Menu/DocManager.java
@@ -0,0 +1,79 @@
+package Menu;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+
+public class DocManager {
+	
+	private DocumentBuilder documentBuilder = null;
+	private Document document = null;
+	private Transformer transformer = null;
+	private String currentDocName = null;
+	
+	public DocManager() {
+		
+		try {
+			documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			transformer = TransformerFactory.newInstance().newTransformer();
+		} catch (javax.xml.parsers.ParserConfigurationException e) {
+			System.err.println(e.getMessage());
+		} catch (TransformerConfigurationException e) {
+			System.err.println(e.getMessage());
+		} catch (TransformerFactoryConfigurationError e) {
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	public Document blankDocument() {
+		document = documentBuilder.newDocument();
+		return this.document;
+	}
+	
+	public boolean load(String docName) {
+		try {
+			File file = new File(docName + ".xml");
+			document = documentBuilder.parse(file);
+			return true;
+		} catch (SAXException e) {
+			System.err.println(e.getMessage());
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+		return false;
+	}
+	
+	public boolean save() {
+		if (currentDocName.isEmpty())
+			return false;
+		saveAs(currentDocName);
+		return true;
+	}
+	
+	public boolean saveAs(String docName) {
+		try {
+			// TODO document has root element ? go on : create and append root element  
+			DOMSource source = new DOMSource(document);
+			StreamResult result = new StreamResult(new File(docName + ".xml"));
+			transformer.transform(source, result);
+			return true;
+		} catch (TransformerException e) {
+			System.err.println(e.getMessage());
+		}
+		return false;
+	}
+	
+}
