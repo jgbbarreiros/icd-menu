be226f05aabf9750f1fd7bf542cc9bdfc6f69342
Joao Barreiros <jgbbarreiros@gmail.com>
2016-03-26 18:02:11 +0000

    Create and remove ingredients
    Home and Ingredient panel

--- src/restaurant/App.java
+++ src/restaurant/App.java
@@ -1,13 +1,23 @@
 package restaurant;
 
 import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Enumeration;
 
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.plaf.FontUIResource;
+
+import org.w3c.dom.Document;
 
 /**
  * Main class
@@ -20,8 +30,12 @@ import javax.swing.plaf.FontUIResource;
 public class App extends javax.swing.JFrame implements Runnable {
 
 	private static final long serialVersionUID = 2102223258041677152L;
-
+	
+	static final String DOCNAME = "restaurant";
+	
 	private FileManager fileManager;
+	private MenuManager menuManager;
+	private Document doc;
 
 	private Font mainFont;
 
@@ -30,7 +44,8 @@ public class App extends javax.swing.JFrame implements Runnable {
 
 	// Ingredients panel
 	private JPanel panelIngredient;
-
+//	private 
+	
 	// Item panel
 	private JPanel panelItem;
 
@@ -58,6 +73,8 @@ public class App extends javax.swing.JFrame implements Runnable {
 	 */
 	private void initVar() {
 		fileManager = new FileManager();
+		doc = fileManager.blank();
+		menuManager = new MenuManager(doc);
 		mainFont = new Font("Segoe UI Light", Font.PLAIN, 18);
 		setUIFont(new FontUIResource(mainFont));
 	}
@@ -98,11 +115,114 @@ public class App extends javax.swing.JFrame implements Runnable {
 	}
 
 	public void initHome() {
-		// TODO construct Home panel
+		panelHome = new JPanel();
+		panelHome.setBackground(Color.DARK_GRAY);
+		getContentPane().add(panelHome, "name_22351689436633");
+		panelHome.setLayout(null);
+		panelHome.setVisible(true);
+		
+		
+		JButton btnIngredients = new JButton("Ingredients");
+		btnIngredients.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				panelHome.setVisible(false);
+				panelIngredient.setVisible(true);
+			}
+		});
+		
+		btnIngredients.setBackground(Color.LIGHT_GRAY);
+		btnIngredients.setBounds(187, 116, 120, 80);
+		panelHome.add(btnIngredients);
+		
+		JButton btnItems = new JButton("Items");
+		btnItems.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panelHome.setVisible(false);
+				panelItem.setVisible(true);
+			}
+		});
+		btnItems.setBackground(Color.LIGHT_GRAY);
+		btnItems.setBounds(406, 116, 120, 80);
+		panelHome.add(btnItems);
+		
+		JButton btnMenu = new JButton("Menu");
+		btnMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panelHome.setVisible(false);
+				panelMenu.setVisible(true);
+			}
+		});
+		btnMenu.setBackground(Color.LIGHT_GRAY);
+		btnMenu.setBounds(111, 293, 492, 80);
+		panelHome.add(btnMenu);
+		
+		JButton btnSave = new JButton("Save");
+		btnSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fileManager.saveAs(DOCNAME);
+			}
+		});
+		btnSave.setBackground(Color.LIGHT_GRAY);
+		btnSave.setBounds(577, 11, 117, 58);
+		panelHome.add(btnSave);
 	}
 
 	public void initIngredient() {
-		// TODO construct Ingredient panel
+		panelIngredient = new JPanel();
+		panelIngredient.setBackground(Color.DARK_GRAY);
+		getContentPane().add(panelIngredient, "name_22353020617238");
+		panelIngredient.setLayout(null);
+		
+		DefaultListModel<String> listMIngredients = new DefaultListModel<String>();
+		JList<String> listIngredients = new JList<String>(listMIngredients);
+		JScrollPane scroll = new JScrollPane(listIngredients);
+		scroll.setBounds(429, 11, 265, 419);
+		panelIngredient.add(scroll);
+		
+		JTextField txtNewIngredient = new JTextField();
+		ActionListener addIngredientAction = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtNewIngredient.selectAll();
+				String ingredientName = txtNewIngredient.getText();
+				if(menuManager.addIngredient(ingredientName))
+					listMIngredients.addElement(ingredientName);
+			}
+		};
+		txtNewIngredient.addActionListener(addIngredientAction);
+		txtNewIngredient.setFont(new Font("Consolas", Font.PLAIN, 14));
+		txtNewIngredient.setText("New Ingredient");
+		txtNewIngredient.setBounds(10, 76, 261, 29);
+		panelIngredient.add(txtNewIngredient);
+		txtNewIngredient.setColumns(10);
+		
+		JButton btnAdd = new JButton("Add");
+		btnAdd.addActionListener(addIngredientAction);
+		btnAdd.setBackground(Color.LIGHT_GRAY);
+		btnAdd.setBounds(295, 65, 113, 43);
+		panelIngredient.add(btnAdd);
+		
+		JButton btnRemove = new JButton("Remove");
+		btnRemove.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String ingredientName = listIngredients.getSelectedValue();
+				if (menuManager.removeIngredient(ingredientName))
+					listMIngredients.removeElement(ingredientName);
+			}
+		});
+		btnRemove.setBackground(Color.LIGHT_GRAY);
+		btnRemove.setBounds(295, 154, 113, 43);
+		panelIngredient.add(btnRemove);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panelIngredient.setVisible(false);
+				panelHome.setVisible(true);
+			}
+		});
+		btnBack.setBackground(Color.LIGHT_GRAY);
+		btnBack.setBounds(10, 366, 113, 64);
+		panelIngredient.add(btnBack);
 	}
 
 	public void initItem() {
--- src/restaurant/MenuManager.java
+++ src/restaurant/MenuManager.java
@@ -1,5 +1,11 @@
 package restaurant;
 
+import java.util.UUID;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
 /**
  * Menu manager
  * 
@@ -7,9 +13,52 @@ package restaurant;
  * to manipulate the DOM and update it self.
  */
 public class MenuManager {
+	
+	private Document doc;
+	private Element rootElement;
+	private Element ingredients;
+	private Element items;
+	private Element menu;
 
-	public MenuManager() {
-		// TODO implement Menu Manager
+	public MenuManager(Document doc) {
+		this.doc = doc;
+		
+		// DOM base structure
+		rootElement = doc.createElement("restaurant");
+		doc.appendChild(rootElement);
+		
+		ingredients = doc.createElement("ingredients");
+		rootElement.appendChild(ingredients);
+		
+		items = doc.createElement("items");
+		rootElement.appendChild(items);
+		
+		menu = doc.createElement("menu");
+		rootElement.appendChild(menu);
+	}
+	
+	public boolean addIngredient(String name) {
+		NodeList ingredientsList = ingredients.getChildNodes();
+		for (int i = 0; i < ingredientsList.getLength(); i++) {
+			if (ingredientsList.item(i).getTextContent().equals(name))
+				return false;
+		}
+		Element ingredient = doc.createElement("ingredient");
+		ingredient.appendChild(doc.createTextNode(name));
+		ingredient.setAttribute("ingrID", UUID.randomUUID().toString());
+		ingredients.appendChild(ingredient);
+		return true;
+	}
+	
+	public boolean removeIngredient(String name) {
+		NodeList ingredientsList = ingredients.getChildNodes();
+		for (int i = 0; i < ingredientsList.getLength(); i++) {
+			if (ingredientsList.item(i).getTextContent().equals(name)) {
+				ingredients.removeChild(ingredientsList.item(i));
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
